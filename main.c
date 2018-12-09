56208e5086bd416990c59093c3d563ee0f2ae654
juanmavelez <45683136+juanmavelez@users.noreply.github.com>
2018-12-09 14:50:43 +0100

    Update main.c
    Se implementa la función de   -->  void enviar_valores_usart()

--- main.c
+++ main.c
@@ -39,8 +39,25 @@ char int_lum_imprimir;
 // ###### USART ######
 
 void enviar_valores_usart() {
-    /* Para mandar por la USART:
-       TXREG = 8-bit-value */
+ 
+    TXSTAbits.BRGH = 0;
+    BAUDCTLbits.BRG16 = 0;
+ 
+    SPBRGH = 0;   //// SPBRGH:SPBRG 
+    SPBRG = 32;  // 9600 baud rate with 20MHz Clock 
+ 
+    TXSTAbits.SYNC = 0; /* Asynchronous */
+    TXSTAbits.TX9 = 0; /* TX 8 data bit */
+ 
+    RCSTAbits.RX9 = 0; /* RX 8 data bit */
+ 
+    PIE1bits.TXIE = 0; /* Disable TX interrupt */
+    PIE1bits.RCIE = 0; /* Disable RX interrupt */
+ 
+    RCSTAbits.SPEN = 1; /* Serial port enable */
+ 
+    TXSTAbits.TXEN = 0; /* Reset transmitter */
+    TXSTAbits.TXEN = 1; /* Enable transmitter */
 }
 
 
