11bd73e807bfe11e709720cfc072740211ce42b8
xam <xayide.rg@gmail.com>
2018-12-21 15:15:34 +0100

    Aumenta el tamaño de tu código en sólo 7 días con este increíble truco

--- main-semi.c
+++ main-semi.c
@@ -10,7 +10,7 @@
 
 char TMR1_contador_5s = 0;
 char TMR0_contador_5s = 0;
-int  resultado_CAD = 0;
+int  resultado_CAD    = 0;
 
 
 char luz_imprimir     = 0; // Luz
@@ -34,22 +34,32 @@ void enviar_usart(void) {
     printf("Hum: [%d] [0x%x]\n\r", hum_imprimir, hum_imprimir);
 }
 
+char consigna_grados(void) {
+    /* Convierte a grados el valor recibido por el CAD cuando leemos la consigna
+       de temperatura. 
+       Voltaje de 0 a 3 V   -> Temperaturas de 17 a 28 grados centigrados
+       Voltaje de 3 a 3.8 V -> Sistema apagado */
+}
+
 
 
 float voltLuz = 0;
 char  luz     = 0;
 void leer_luz(void) {
-    // Leemos de la patita AN3
-    //while (ADCON0bits.GO_DONE);
+    // Leemos el pin AN3 (RA3)
     ADCON0bits.CHS0    = 1;
     ADCON0bits.CHS1    = 1;
     ADCON0bits.CHS2    = 0;
     ADCON0bits.CHS3    = 0;
-    
+    // Arrancamos el CAD
     ADCON0bits.GO_DONE = 1;
-    
+    // Esperamos a que termine la conversion
     while(ADCON0bits.GO_DONE);
-
+    // Limpiamos los valores
+    voltLuz = 0;
+    luz     = 0;
+    // Guardamos en resultado_CAD el resultado de la conversion
+    resultado_CAD = 0;
     resultado_CAD = ADRESH << 8;
     resultado_CAD = resultado_CAD + ADRESL;
 
@@ -64,20 +74,19 @@ void leer_luz(void) {
 float voltHumedad = 0;
 float porcentaje  = 0;
 void leer_humedad(void) {
-    // Leemos la patita AN2
-    //while (ADCON0bits.GO_DONE);
+    // Leemos la el pin AN2 (RA2)
     ADCON0bits.CHS0    = 0;
     ADCON0bits.CHS1    = 1;
     ADCON0bits.CHS2    = 0;
     ADCON0bits.CHS3    = 0;
-    
+    // Arrancamos el CAD
     ADCON0bits.GO_DONE = 1;
-    
+    // Esperamos a que la conversion termine
     while(ADCON0bits.GO_DONE);
-
+    // Limpiamos los valores
     voltHumedad = 0;
     porcentaje  = 0;
-
+    // Guardamos en resultado_CAD el resultado de la conversion
     resultado_CAD = 0;
     resultado_CAD = ADRESH << 8;
     resultado_CAD = resultado_CAD + ADRESL;
@@ -93,17 +102,19 @@ void leer_humedad(void) {
 float voltExterior = 0;
 char  tempExterior = 0;
 void leer_tem_x(void) {
-    // Decidle que usamos la patita AN1 (RA1) (PIC16F886 pag 106)
-    //while (ADCON0bits.GO_DONE);
+    // Leemos el pin AN1 (RA1)
     ADCON0bits.CHS0    = 1;
     ADCON0bits.CHS1    = 0;
     ADCON0bits.CHS2    = 0;
     ADCON0bits.CHS3    = 0;
-    
+    // Arrancamos el CAD
     ADCON0bits.GO_DONE = 1;
-    
+    // Esperamos a que la conversion termine
     while(ADCON0bits.GO_DONE);
-
+    // Limpiamos los valores
+    voltExterior = 0;
+    tempExterior = 0;
+    // Guardamos en resultado_CAD el resultado de la conversion
     resultado_CAD = 0;
     resultado_CAD = ADRESH << 8;
     resultado_CAD = resultado_CAD + ADRESL;
@@ -119,20 +130,19 @@ void leer_tem_x(void) {
 float voltInterior = 0;
 char  tempInterior = 0;
 void leer_tem_i(void) {
-    // Decidle que usamos la patita AN0 (RA0) (PIC16F886 pag 106)
-    //while (ADCON0bits.GO_DONE);
+    // Leemos del pin AN0 (RA0)
     ADCON0bits.CHS0    = 0;
     ADCON0bits.CHS1    = 0;
     ADCON0bits.CHS2    = 0;
     ADCON0bits.CHS3    = 0;
-    
+    // Arrancamos el CAD
     ADCON0bits.GO_DONE = 1;
-    
+    // Esperamos a que la conversion termine
     while(ADCON0bits.GO_DONE);
-    
+    // Limpiamos los valores
     voltInterior = 0;
     tempInterior = 0;
-
+    // Guardamos en resultado_CAD el resultado de la conversion
     resultado_CAD = 0;
     resultado_CAD = ADRESH << 8;
     resultado_CAD = resultado_CAD + ADRESL;
@@ -146,21 +156,23 @@ void leer_tem_i(void) {
 }
 
 void leer_consigna(void) {
+    // Leemos el pin AN5 (RA5))
     ADCON0bits.CHS0    = 1;
     ADCON0bits.CHS1    = 0;
     ADCON0bits.CHS2    = 1;
     ADCON0bits.CHS3    = 0;
-    
+    // Arrancamos el CAD
     ADCON0bits.GO_DONE = 1;
-    
+    // ESperamos a que la conversion termine
     while(ADCON0bits.GO_DONE);
-    
+    // Guardamos en resultado_CAD el resultado de la conversion
     resultado_CAD = 0;
     resultado_CAD = ADRESH << 8;
     resultado_CAD = resultado_CAD + ADRESL;
     
-    if (resultado_CAD != dit_imprimir) { // lo que lee del CAD no coincide con el último valor: ha cambiado
-        //dit_imprimir = convertir_a_grados(resultado_CAD); // Actualizamos el valor a imprimir. Hay que convertir a grados centigrados
+    
+    if (resultado_CAD != dit_imprimir) { // lo que lee del CAD no coincide con el ultimo valor: ha cambiado
+        //dit_imprimir = consigna_grados(); // Actualizamos el valor a imprimir. Hay que convertir a grados centigrados
         // Trampa
         dit_imprimir++;
         TMR0_contador_5s = 0;             // Ponemos el contador a 0 para que cuente 5s desde ahora
@@ -177,7 +189,7 @@ void evaluar_consigna(void) {
         // Activar refrigerador
         // Activar ventilador con los valores que sean
     }
-    else {
+    else { // Son iguales
        // continue; // Do nothing
     }
 }
@@ -185,10 +197,11 @@ void evaluar_consigna(void) {
 
 
 void TMR0_interrupt_handler(void) {
+    TMR0 = 61; // Para que mantenga el valor
     TMR0_contador_5s++;
     if (TMR0_contador_5s == 500) {
-        /* Han pasado 5 segundos desde que dejaron quieto el dial de temperatura */
-        /* Mandamos las cosas al refrigerador/calefactor y ventilador */
+        /* Han pasado 5 segundos desde que dejaron quieto el dial de temperatura
+           Mandamos las cosas al refrigerador/calefactor y ventilador */
         evaluar_consigna();
         TMR0_contador_5s = 0; // Lo ponemos a cero de nuevo
     }
@@ -196,11 +209,11 @@ void TMR0_interrupt_handler(void) {
 
 void TMR1_interrupt_handler(void) {
     TMR1_contador_5s++;
-    if (TMR1_contador_5s % 5 == 0) {
+    if (TMR1_contador_5s % 5 == 0) { // Contar 0.5s
         // printf("TMR1 ha contado 0.5s. Leemos Consigna\n\r");
         leer_consigna();
    }
-    if (TMR1_contador_5s == 50) {
+    if (TMR1_contador_5s == 50) {    // Contar 5s
         // printf("TMR1 ha contado 5s. Imprimimos sensor\n\r");
         leer_luz();
         leer_humedad();
@@ -212,7 +225,7 @@ void TMR1_interrupt_handler(void) {
 }
 
 void interrupt general_interrupt_handler(void) {
-    if (PIR1bits.ADIF) {   // Interrupt del CAD
+    if (PIR1bits.ADIF) {   // Interrupt del CAD.
         PIR1bits.ADIF = 0; // Limpiamos el flag de interrupcion del CAD
     }
     else if (PIR1bits.TMR1IF) { // TMR1 interrumpe cada 0.1s
@@ -227,21 +240,31 @@ void interrupt general_interrupt_handler(void) {
 
 
 
+void set_interrupts(void) {
+    INTCONbits.GIE  = 1; // General Interrupt Enable
+    INTCONbits.PEIE = 1; // PEripheral Interrupt Enable
+    INTCONbits.T0IE = 1; // Allow TMR0 interrupts
+    PIE1bits.TMR1IE = 1; // Allow TMR1 interrupts
+    PIE1bits.ADIE   = 1; // Allow AD interrupts (CAD)
+
+    PIE1bits.TXIE   = 0; // Don't allow USART TX interrupts
+    PIE1bits.RCIE   = 0; // Don't allow USART RX interrupts
+}
 
 void init_CAD(void) {
     
-    // Decidle quï¿½ frecuencia usar (01 = Fosc/8)
+    // LE decimos que frecuencia usar (01 = Fosc/8)
     ADCON0bits.ADCS0 = 1;
     ADCON0bits.ADCS1 = 0;
 
-    // ADCON1 tambiï¿½n configura el CAD
+    // ADCON1 tambien configura el CAD
     ADCON1bits.ADFM  = 1; // Justify right
     ADCON1bits.VCFG0 = 0; // Cosas de voltajes parte 1: El comienzo de VSS
     ADCON1bits.VCFG1 = 0; // Cosas de voltajes parte 2: VDD Returns
 
     // Arrancar el CAD: (ADON lo enciende, GO_DONE le dice que empiece a currar)
     ADCON0bits.ADON    = 1;
-    // ADCON0bits.GO_DONE = 1;
+    // ADCON0bits.GO_DONE = 1; // Lo hacemos en las funciones propias de leer_xxx
 
 }
 
@@ -265,20 +288,20 @@ void init_TMR1(void) {
     TMR1L = 220;            // == 11011100
     PIR1bits.TMR1IF   = 0;  // Limpiamos la flag por si acaso
 }
-
+    
 void init_USART(void) {
 
     BAUDCTLbits.BRG16  = 0; // No transmitimos en 16 bits
 
     /* ###### SPBRGH:SPBRG ###### */
     SPBRGH = 0;
-    SPBRG  = 32; // Ver la tablita, creo que deberï¿½a ser 32 para un valor de 9600
+    SPBRG  = 32; // 32 para un valor de 9600 baudios
 
     /* ###### TXSTA ###### */
     TXSTAbits.TX9  = 0; // 8-bit transmission
     TXSTAbits.TXEN = 0; // Transmit disabled (purpose is resetting it)
     TXSTAbits.TXEN = 1; // Transmit enabled
-    TXSTAbits.SYNC = 0; // Modo asï¿½ncrono
+    TXSTAbits.SYNC = 0; // Modo asincrono
     TXSTAbits.BRGH = 0; // No queremos High Baud Rate para transmitir
 
     /* ###### RCSTA ###### */
@@ -286,17 +309,14 @@ void init_USART(void) {
     RCSTAbits.RX9  = 0; // 8-bit transmission
 }
 
-void set_interrupts(void) {
-    INTCONbits.GIE  = 1; // General Interrupt Enable
-    INTCONbits.PEIE = 1; // PEripheral Interrupt Enable
-    INTCONbits.T0IE = 1; // Allow TMR0 interrupts
-    PIE1bits.TMR1IE = 1; // Allow TMR1 interrupts
-    PIE1bits.ADIE   = 1; // Allow AD interrupts (CAD)
-
-    PIE1bits.TXIE   = 0; // Don't allow USART TX interrupts
-    PIE1bits.RCIE   = 0; // Don't allow USART RX interrupts
+void init_portA(void) {
+    TRISA = 0b00101111; // Los pines 0, 1, 2, 3 y 5 en modo input
+    ANSEL = 0b00101111; // Los pines 0, 1, 2, 3 y 5 en modo analogico
 }
 
+void init_portC(void) {
+    TRISC  = 0b00000000; // Todos los pines en modo output (usamos los pines 1, 2 y 4)
+}
 
 
 void main(void) {
@@ -307,6 +327,8 @@ void main(void) {
     init_CAD();
     init_TMR1();
     init_TMR0();
+    init_portA();
+    init_portC();
     
     
     printf("##### BANNER ######\n\r");
